a1cadca1b0241ddb0b9fb682bc052b5fec8e754d
Sahil Kumar <sahil511kumar@gmail.com>
2025-06-16 02:21:28 +0530

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,jsx,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2024,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      js,
+      react: pluginReact,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      ...reactHooks.configs["recommended-latest"].rules,
+
+      // ✅ Custom rules
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  {
+    files: ["*.config.js", "*.config.mjs", "vite.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        __dirname: "readonly",
+        __filename: "readonly",
+      },
+    },
+  },
+]);
