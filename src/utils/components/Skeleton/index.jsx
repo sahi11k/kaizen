954dfa27a6e3f301e2850b1fe3ea53bd85fb1925
Sahil Kumar <sahil511kumar@gmail.com>
2025-06-17 02:21:25 +0530

    add skeleton and toaster

--- src/utils/components/Skeleton/index.jsx
+++ src/utils/components/Skeleton/index.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import SkeletonComponent, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import styles from "./style.module.css";
 
-const Skeleton = () => {
-  return <div>Skeleton</div>;
+const Skeleton = (props) => {
+  return (
+    <SkeletonTheme
+      baseColor="var(--surface)"
+      highlightColor="var(--accent-primary)"
+    >
+      <SkeletonComponent
+        {...props}
+        containerClassName={styles.skeleton__container}
+      />
+    </SkeletonTheme>
+  );
 };
 
 export default Skeleton;
