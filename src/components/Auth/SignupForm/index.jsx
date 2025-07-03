912853a2d5da473ab13435a6482d85602cd9cc14
Sahil Kumar <sahil511kumar@gmail.com>
2025-07-04 02:16:52 +0530

    add form validation and supabase auth

--- src/components/Auth/SignupForm/index.jsx
+++ src/components/Auth/SignupForm/index.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import styles from "@/components/Auth/style.module.css";
 import { Link } from "react-router";
 import FormItem from "@/utils/components/FormItem";
+import { ErrorText } from "@/components/Auth/ErrorText";
+import { signUpNewUser } from "@/db/apis/auth";
 
 const SignupForm = () => {
   const [formValues, setFormValues] = useState({
@@ -10,39 +12,75 @@ const SignupForm = () => {
     password: "",
   });
 
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const hasErrors = Object.values(errors).some((error) => error !== "");
+    if (hasErrors) return;
+    const response = await signUpNewUser(formValues);
+    console.info({ response });
+  };
+
+  const handleChange = (field, value) => {
+    const error = validateField(field, value);
+    setErrors({ ...errors, [field]: error });
+    setFormValues({ ...formValues, [field]: value });
+  };
+
+  const validateField = (field, value) => {
+    const trimmedValue = value?.trim();
+    switch (field) {
+      case "name":
+        if (!trimmedValue || !trimmedValue?.length)
+          return "Please enter a valid name";
+        if (trimmedValue?.length > 50)
+          return "Name must be less than 50 characters";
+        return "";
+      case "email":
+        if (trimmedValue?.includes("@") && trimmedValue?.includes("."))
+          return "";
+        return "Please enter a valid email";
+      case "password":
+        if (trimmedValue?.length >= 6 && trimmedValue?.length <= 32) return "";
+        return "Password must be between 6 and 32 characters";
+    }
+  };
+
   return (
     <div className={styles.loginForm}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormItem.Input
           type="text"
           id="signupName"
           value={formValues.name}
-          onChange={(e) =>
-            setFormValues({ ...formValues, name: e.target.value })
-          }
+          onChange={(e) => handleChange("name", e.target.value)}
           required
           placeholder="Enter name"
         />
+        {errors.name && <ErrorText error={errors.name} />}
         <FormItem.Input
           type="email"
           id="signupEmail"
           value={formValues.email}
-          onChange={(e) =>
-            setFormValues({ ...formValues, email: e.target.value })
-          }
+          onChange={(e) => handleChange("email", e.target.value)}
           required
           placeholder="Enter email"
         />
+        {errors.email && <ErrorText error={errors.email} />}
         <FormItem.Input
           type="password"
           id="signupPassword"
           value={formValues.password}
-          onChange={(e) =>
-            setFormValues({ ...formValues, password: e.target.value })
-          }
+          onChange={(e) => handleChange("password", e.target.value)}
           required
           placeholder="Enter password"
         />
+        {errors.password && <ErrorText error={errors.password} />}
         <button type="submit" className={`btn ${styles.submitButton}`}>
           Sign Up
         </button>
--- src/db/apis/auth.js
+++ src/db/apis/auth.js
@@ -1,13 +1,33 @@
 import { supabase } from "@/db/supabase";
 
-export async function signInWithEmail(email) {
-  const response = await supabase.auth.signInWithOtp({
-    email,
+export async function signUpNewUser(payload) {
+  const response = await supabase.auth.signUp({
+    email: payload.email,
+    password: payload.password,
     options: {
-      shouldCreateUser: false,
-      //   emailRedirectTo: "https://example.com/welcome",
+      emailRedirectTo: `${import.meta.env.VITE_APP_URL}/auth/callback`,
+      data: {
+        display_name: payload.name,
+      },
     },
   });
+  return response;
+}
 
+export async function signInWithEmail(payload) {
+  const response = await supabase.auth.signInWithPassword({
+    email: payload.email,
+    password: payload.password,
+  });
+  return response;
+}
+
+// export async function signOut() {
+//   const response = await supabase.auth.signOut();
+//   return response;
+// }
+
+export async function getUserSession() {
+  const response = await supabase.auth.getSession();
   return response;
 }
