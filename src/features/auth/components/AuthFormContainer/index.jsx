e8a5a5b5f34b9b953b125828d70766f2d29ceafe
Sahil Kumar <sahil511kumar@gmail.com>
2026-07-16 00:40:54 +0530

    ui : make auth page responsive

--- src/features/auth/components/AuthFormContainer/index.jsx
+++ src/features/auth/components/AuthFormContainer/index.jsx
@@ -3,7 +3,7 @@ import { Logo } from "@/shared/ui";
 const AuthFormContainer = ({ headerText, children }) => {
   return (
     <div className="flex items-center justify-center min-h-screen sm:p-6">
-      <div className="gap-6 flex flex-col flex-1 border-border sm:border lg:border-none max-w-md rounded-lg lg:rounded-none sm:px-4 sm:py-6">
+      <div className="gap-6 flex flex-col flex-1 max-w-md sm:px-4 sm:py-6">
         <div className="flex justify-center items-center flex-col gap-2 xl:gap-4">
           <Logo
             showText={false}
